035c8c2c34138bb898561e78d9e0ad14fdf732a6
Andrei <andrei.hudalla@outlook.com>
2023-06-24 14:38:40 -0500

    Minimal design changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,13 @@ export default defineNuxtConfig({
         "@nuxtjs/i18n",
         '@nuxthq/ui',
         'nuxt-icon',
-        '@nuxt/content'
+        '@nuxt/content',
+        'nuxt-appwrite'
     ],
+    appwrite: {
+      project: "echo.mods",
+      endpoint: "https://cloud.appwrite.io/v1"
+    },
     i18n: {
         strategy: "no_prefix",
         locales: ["ru", "en"], // used in URL path prefix
