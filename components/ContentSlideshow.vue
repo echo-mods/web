3983368df5e9fd1cf3a8aa14d9832f89ca9ae4a3
Andrei <andrei.hudalla@outlook.com>
2023-08-21 10:39:54 -0500

    Hugely improved slideshow component

--- components/ContentSlideshow.vue
+++ components/ContentSlideshow.vue
@@ -1,19 +1,45 @@
 <script setup lang="ts">
+const root = ref()
+const enabled = ref()
+const controls = ref()
+const controlsDefaultWidth = ref(0)
+
+const btn_left = ref()
+const btn_right = ref()
 
 const slideID = ref(0)
-
 const props = defineProps(["content"])
+
+const cooldown = ref(false)
 
 const transitionName = ref("right")
 
 const data = computed(() => { if (props.content) { return props.content[(slideID.value + props.content.length * 2) % props.content.length] } })
 
 const incrementSlide = (value: number) => {
-  transitionName.value = value === -1 ? "right" : "left"
+  if (cooldown.value) { return }
+  cooldown.value = true
+  const directionString: string = value === -1 ? "right" : "left"
+  transitionName.value = directionString
+  switch (directionString) {
+    case "right":
+      btn_right.value.classList.add("active")
+      break;
+    case "left":
+      btn_left.value.classList.add("active")
+      break;
+  }
   slideID.value += value
   if (props.content && slideID.value < 0) {
     slideID.value = props.content?.length + slideID.value
   }
+  setTimeout(() => {
+    btn_left.value.classList.remove("active")
+    btn_right.value.classList.remove("active")
+  }, 300);
+  setTimeout(() => {
+    cooldown.value = false
+  }, 450);
 }
 
 const setSlide = (value: number) => {
@@ -24,12 +50,33 @@ const setSlide = (value: number) => {
 const trimmedID = computed(() => (slideID.value + (props.content || "").length * 100000) % (props.content || "").length)
 
 const handleKeypress = (event: any) => {
+  if (!enabled.value) { return }
   if (event.key === "ArrowRight") {
     incrementSlide(1)
   } else if (event.key === "ArrowLeft") {
     incrementSlide(-1)
   }
 }
+
+const updateHoverState = () => {
+  enabled.value = root.value.matches(":hover")
+}
+
+
+watchEffect(() => {
+  if (!root.value) { return }
+  if (controls.value && controlsDefaultWidth.value === 0) {
+    controlsDefaultWidth.value = controls.value.offsetWidth
+    enabled.value = false
+  }
+  root.value.addEventListener("mouseover", updateHoverState)
+  root.value.addEventListener("mouseout", updateHoverState)
+})
+
+const controlsCurrentWidth = computed(() => {
+  const normalCalculation = enabled.value ? controlsDefaultWidth.value : controlsDefaultWidth.value * 0.5
+  return controlsDefaultWidth.value === 0 ? "auto" : `${normalCalculation}px`
+})
 
 onMounted(() => {
   window.addEventListener("keydown", handleKeypress)
@@ -42,15 +89,23 @@ onUnmounted(() => {
 
 <template>
   <ClientOnly>
-    <div class="content-container">
+    <div class="content-container" ref="root">
       <!-- Slidshow controls -->
-      <div class="slideshow-controls">
-        <button class="right" @click="incrementSlide(-1)">&lt;</button>
+      <div class="slideshow-controls" ref="controls" :style="{ width: controlsCurrentWidth }">
+        <Transition name="controls-button">
+          <button ref="btn_right" v-if="enabled !== false" class="right" @click="incrementSlide(-1)">
+            <Icon name="line-md:arrow-left" />
+          </button>
+        </Transition>
         <div class="dots">
           <div class="dot" v-for="(data, index) in content" @click="setSlide(index)"
             :class="{ selected: index === trimmedID }"></div>
         </div>
-        <button class="left" @click="incrementSlide(1)">></button>
+        <Transition name="controls-button">
+          <button ref="btn_left" v-if="enabled !== false" class="left" @click="incrementSlide(1)">
+            <Icon name="line-md:arrow-left" />
+          </button>
+        </Transition>
       </div>
       <!-- Content placeholders -->
       <TransitionGroup :name="transitionName">
@@ -87,6 +142,7 @@ onUnmounted(() => {
   }
 
   .slideshow-controls {
+    translate: 0 150%;
     position: absolute;
     left: 50%;
     bottom: 10%;
@@ -101,10 +157,43 @@ onUnmounted(() => {
     padding: 1rem 0.5rem;
     border: 2px rgba(255, 255, 255, 0.3) solid;
     border-radius: 8px;
+    opacity: 0;
+    transition: all 0.3s ease-in-out;
+    overflow: hidden;
+
+    .filler {
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      width: 50%;
+      background-color: white;
+      opacity: 0.1;
+    }
 
     >button {
       width: 2rem;
       font-size: 0.75rem;
+      transition: all 0.15s;
+      margin-bottom: 0.1rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
+    
+    >button.active {
+      transition: all 0.15s;
+      scale: 1.2;
+      translate: 30% 0;
+      &.right {
+        translate: -30% 0;
+      }
+    }
+
+    >button.left > svg {
+      -webkit-transform: scaleX(-1);
+      transform: scaleX(-1);
     }
 
     .dots {
@@ -113,16 +202,29 @@ onUnmounted(() => {
     }
 
     .dot {
-      width: 0.3rem;
+      opacity: 0;
+      width: 0;
       aspect-ratio: 1 !important;
       border-radius: 1rem;
       cursor: pointer;
       outline: 1px rgba(255, 255, 255, 0.3) solid;
-      transition: all 0.6s;
+      transition: all 0.15s;
     }
 
     .dot.selected {
       background-color: white;
+    }
+  }
+
+  &:hover {
+    .slideshow-controls {
+      opacity: 1;
+      translate: 0 0;
+    }
+
+    .dot {
+      opacity: 1;
+      width: 0.3rem;
     }
   }
 }
@@ -164,7 +266,7 @@ onUnmounted(() => {
 }
 
 .right-enter-from {
-  opacity: 0;
+  opacity: 0.85;
   transform: translateX(-100%);
 }
 
@@ -173,7 +275,7 @@ onUnmounted(() => {
 }
 
 .right-leave-to {
-  opacity: 0;
+  opacity: 0.85;
   transform: translateX(100%);
 }
 
@@ -192,7 +294,7 @@ onUnmounted(() => {
 }
 
 .left-enter-from {
-  opacity: 0;
+  opacity: 0.85;
   transform: translateX(100%);
 }
 
@@ -201,7 +303,7 @@ onUnmounted(() => {
 }
 
 .left-leave-to {
-  opacity: 0;
+  opacity: 0.85;
   transform: translateX(-100%);
 }
 
@@ -209,9 +311,18 @@ onUnmounted(() => {
   position: absolute;
 }
 
+.controls-button-enter-active,
+.controls-button-leave-active {
+  transition: all 0.3s;
+}
+
+.controls-button-enter-from,
+.controls-button-leave-to {
+  opacity: 0;
+}
+
 @media (max-width: 600px) {
   .content-container {
     width: 100%;
   }
-}
-</style>
+}</style>
