02930964836a91b6739a9961b82e9af045f8d18a
Andrei <andrei.hudalla@outlook.com>
2023-12-10 14:45:37 -0600

    Added description

--- app.config.ts
+++ app.config.ts
@@ -12,5 +12,6 @@ export default defineAppConfig({
     ],
     frontpage_news: [
         "development"
-    ]
+    ],
+	description: "Empowering the S.T.A.L.K.E.R. modding community with an independent platform. Your hub for high-quality modding resources, bridging innovation beyond GSC Game World. #STALKERModding #Innovation"
 });
